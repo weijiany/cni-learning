4a75382340fcf582ff1e18aecb3a4c610e282aea
weijiany <weijian.yang@thoughtworks.com>
2024-01-27 20:13:08 +0800

    parse stdin input.

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/cni/pkg/types"
 	"github.com/containernetworking/cni/pkg/version"
 	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
 	"my-cni/utils"
 )
 
+type PluginConfig struct {
+	types.NetConf
+
+	Bridge string `json:"bridge"`
+	Subnet string `json:"subnet"`
+}
+
 func cmdAdd(args *skel.CmdArgs) error {
-	utils.WriteLog(args.ContainerID)
-	utils.WriteLog(args.Netns)
-	utils.WriteLog(args.IfName)
-	utils.WriteLog(args.Args)
-	utils.WriteLog(args.Path)
+	pluginConfig := &PluginConfig{}
+	err := json.Unmarshal(args.StdinData, pluginConfig)
+	if err != nil {
+		panic(err)
+	}
 	return nil
 }
 
